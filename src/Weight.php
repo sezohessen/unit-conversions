5c4932c84b8a19c0467f3598d0451d55633212a6
sezohessen <hu.elshazly@nu.edu.eg>
2022-10-17 17:14:35 +0200

    Weight unit test

---
+++ src/Weight.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace sezohessen\conversions;
+
+class Weight
+{
+    private float $kilo;
+    public static function fromKilo(float $kilo): self
+    {
+        return new static ($kilo);
+    }
+    public function __construct(float $kilo)
+    {
+        $this->kilo = $kilo;
+    }
+    public function toLbs(): float
+    {
+        return $this->kilo * 2.20462262;
+    }
+}
---
+++ tests/WeightTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace sezohessen\conversions\Tests;
+use PHPUnit\Framework\TestCase;
+use sezohessen\conversions\Weight;
+
+class WeightTest extends TestCase  {
+    /** @test **/
+    public function it_can_convert_kilograms_to_lbs()
+    {
+        $lbs = Weight::fromKilo(100)->toLbs();
+        $this->assertEqualsIgnoringCase(220.46226199999998,$lbs);
+    }
+}
