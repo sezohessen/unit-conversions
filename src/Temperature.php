833cd26580d36a290ebdd153010a9e3eb08eb5fa
sezohessen <hu.elshazly@nu.edu.eg>
2022-10-17 20:10:56 +0200

    Temp Conversion

---
+++ src/Temperature.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace sezohessen\conversions;
+
+class Temperature
+{
+    private float $celsius;
+
+    public static function fromCelsius(float $celsius): self
+    {
+        return new static ($celsius);
+    }
+    public function __construct(float $celsius)
+    {
+        $this->celsius = $celsius;
+    }
+    public function toFahrenheit(): float
+    {
+        return ($this->celsius * 1.8) + 32;
+    }
+}
